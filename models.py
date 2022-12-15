9d7a6b9f8728ccfdfb85f8cde3a5008ab4ae895b
Cam <17ctb3@queensu.ca>
2022-12-15 15:08:00 -0500

    Ensured consistency between hyperparameter tuning metrics

--- models.py
+++ models.py
@@ -1,36 +1,13 @@
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
 from sklearn import svm
-from multiprocessing import cpu_count
 from sklearn.metrics import f1_score, roc_auc_score
 import time
 from hyperopt import STATUS_OK, Trials, fmin, tpe
-from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold, cross_val_score
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_score
 import numpy as np
 
-def train_svm(X, y, params: dict) -> svm.SVC:
-    """_summary_
-
-    Args:
-        X (_type_): _description_
-        y (_type_): _description_
-        params (dict): _description_
-        modelDir (str, optional): _description_. Defaults to "models/".
-        modelName (str, optional): _description_. Defaults to "svm".
-
-    Returns:
-        svc: trained SVM model for classification
-    """       
-    svc = svm.SVC(**params)
-    
-    print("Training SVM model...")
-    
-    svc.fit(X, y)
-    
-    return svc
-
-
-def tune_svm(X, y, param_grid: dict, cv: int = 2) -> svm.SVC:
+def tune_svm(X, y,  param_grid: dict, isMulticlass: bool = False, cv: int = 2) -> svm.SVC:
     """Tune SVM model hyperparameters using GridSearchCV and sklearn API.
 
     Args:
@@ -44,8 +21,11 @@ def tune_svm(X, y, param_grid: dict, cv: int = 2) -> svm.SVC:
     """  
     #create SVM model for tuning
     svm_model = svm.SVC()
-
-    grid = GridSearchCV(svm_model, param_grid=param_grid, cv=cv, verbose=1, return_train_score=True)
+    if isMulticlass:
+        grid = GridSearchCV(svm_model, scoring='f1_weighted',param_grid=param_grid, cv=cv, verbose=1, return_train_score=True)
+    else:
+        grid = GridSearchCV(svm_model, scoring='roc_auc', param_grid=param_grid, cv=cv, verbose=1, return_train_score=True)
+    
     grid.fit(X, y)
 
     # Print the best parameters and score
